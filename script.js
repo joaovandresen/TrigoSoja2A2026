76f5aaa07402da5bb51ae5b23953c4317a3f7b73
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2026-08-31 11:13:34 +0000

    aula da segunda

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const botao = document.getElementById("botao-tema");
 
+botao.onclick = function() {
+    document.body.classList.toggle("tema-escuro");
+    
+    if (botao.textContent == "🌙") {
+        botao.textContent = "☀️";
+    } else {
+        botao.textContent = "🌙";
+    }
+};
